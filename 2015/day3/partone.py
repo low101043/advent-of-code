3764f58d821a03b98d1b2c98d632dd9ea87e3869
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-01-07 12:10:21 +0000

    Added Day 3 2015 Solutions

---
+++ 2015/day3/partone.py
@@ -0,0 +1,35 @@
+import csv
+import os
+
+def partone(input):
+    
+    houses_visited = set([(0,0)])
+    current_pos = [0,0]
+    for letter in input:
+        if letter == "^":
+            y_axis = current_pos[1]
+            current_pos[1] = y_axis + 1
+        elif letter == ">":
+            x_axis = current_pos[0]
+            current_pos[0] = x_axis + 1
+        elif letter == "v":
+            y_axis = current_pos[1]
+            current_pos[1] = y_axis - 1
+        elif letter == "<":
+            x_axis = current_pos[0]
+            current_pos[0] = x_axis - 1
+        else:
+            print("ERROR")
+        
+        tuple_data = tuple(current_pos)
+        houses_visited.add(tuple_data)
+
+    return len(houses_visited)
+        
+
+print(os.getcwd())
+os.chdir("/home/low101043/Documents/adventOfCode/solutions/2015/day3")
+with open("input.txt") as data:
+    file_to_read = data.read()
+
+print(partone(file_to_read))
---
+++ 2015/day3/parttwo.py
@@ -0,0 +1,87 @@
+import csv
+import os
+
+def partone(input):
+    
+    houses_visited = set([(0,0)])
+    current_pos = [0,0]
+    for letter in input:
+        if letter == "^":
+            y_axis = current_pos[1]
+            current_pos[1] = y_axis + 1
+        elif letter == ">":
+            x_axis = current_pos[0]
+            current_pos[0] = x_axis + 1
+        elif letter == "v":
+            y_axis = current_pos[1]
+            current_pos[1] = y_axis - 1
+        elif letter == "<":
+            x_axis = current_pos[0]
+            current_pos[0] = x_axis - 1
+        else:
+            print("ERROR")
+        
+        tuple_data = tuple(current_pos)
+        houses_visited.add(tuple_data)
+
+    return len(houses_visited)
+        
+
+def parttwo(input):
+
+    houses_visited = set([(0,0)])
+    current_pos_fc = [0,0]
+    current_pos_rfc = [0,0]
+
+    fc = True
+
+    for letter in input:
+        
+        if fc == True:
+            if letter == "^":
+                y_axis = current_pos_fc[1]
+                current_pos_fc[1] = y_axis + 1
+            elif letter == ">":
+                x_axis = current_pos_fc[0]
+                current_pos_fc[0] = x_axis + 1
+            elif letter == "v":
+                y_axis = current_pos_fc[1]
+                current_pos_fc[1] = y_axis - 1
+            elif letter == "<":
+                x_axis = current_pos_fc[0]
+                current_pos_fc[0] = x_axis - 1
+            else:
+                print("ERROR")
+
+            tuple_data = tuple(current_pos_fc)
+            houses_visited.add(tuple_data)
+        else:
+            if letter == "^":
+                y_axis = current_pos_rfc[1]
+                current_pos_rfc[1] = y_axis + 1
+            elif letter == ">":
+                x_axis = current_pos_rfc[0]
+                current_pos_rfc[0] = x_axis + 1
+            elif letter == "v":
+                y_axis = current_pos_rfc[1]
+                current_pos_rfc[1] = y_axis - 1
+            elif letter == "<":
+                x_axis = current_pos_rfc[0]
+                current_pos_rfc[0] = x_axis - 1
+            else:
+                print("ERROR")
+
+            tuple_data = tuple(current_pos_rfc)
+            houses_visited.add(tuple_data)
+
+        fc = not fc
+
+    return len(houses_visited)
+
+print(os.getcwd())
+os.chdir("/home/low101043/Documents/adventOfCode/solutions/2015/day3")
+with open("input.txt") as data:
+    file_to_read = data.read()
+
+print(partone(file_to_read))
+print(parttwo(file_to_read))
