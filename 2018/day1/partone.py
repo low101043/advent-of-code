8f7fb7d69f274ea10586db03f65c6dc7bf993ab3
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-06-08 13:56:49 +0100

    Added day 2 solutions

--- 2018/day1/partone.py
+++ 2018/day1/partone.py
@@ -9,30 +9,9 @@ def partone(input):
     
     return frequency
 
-def parttwo(input):
-
-
-    input_split = input.split("\n")
-    frequency = 0
-    frequencies_seen = set([])
-
-    i = 0 
-    found = False
-
-    while (not found):
-        change = int(input_split[i])
-        frequency += change
-
-        if frequency in frequencies_seen:
-            return frequency
-        else:
-            frequencies_seen.add(frequency)
-
-        i = (i + 1) % len(input_split)
 
 print(os.getcwd())
 with open("2018/day1/input.txt") as data:
     file_to_read = data.read()
 
 print(partone(file_to_read))
-print(parttwo(file_to_read))
---
+++ 2018/day1/parttwo.py
@@ -0,0 +1,38 @@
+import os
+
+def partone(input):
+    frequency = 0
+
+    for number in input.split("\n"):
+        num = int(number)
+        frequency += num
+    
+    return frequency
+
+def parttwo(input):
+
+
+    input_split = input.split("\n")
+    frequency = 0
+    frequencies_seen = set([])
+
+    i = 0 
+    found = False
+
+    while (not found):
+        change = int(input_split[i])
+        frequency += change
+
+        if frequency in frequencies_seen:
+            return frequency
+        else:
+            frequencies_seen.add(frequency)
+
+        i = (i + 1) % len(input_split)
+
+print(os.getcwd())
+with open("2018/day1/input.txt") as data:
+    file_to_read = data.read()
+
+print(partone(file_to_read))
+print(parttwo(file_to_read))
